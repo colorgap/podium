ede46a27dd874eb0b8c3cdd6d588eeec4af812b3
colorgap <admin@colorgap.com>
2015-11-22 12:20:27 -0500

    fix for toggle

--- dist/js/app.js
+++ dist/js/app.js
@@ -65,5 +65,9 @@
 
     angular.module('podium').controller('leftNavCtrl', ['$scope','docsConfig',function($scope,docsConfig){
         $scope.leftNavList = docsConfig.docs;
+
+        $scope.toggleLeftNav = function(){
+            $scope.toggle = !$scope.toggle;
+        };
     }]);
 })();
--- src/app/leftNav/leftnav.controller.js
+++ src/app/leftNav/leftnav.controller.js
@@ -3,5 +3,9 @@
 
     angular.module('podium').controller('leftNavCtrl', ['$scope','docsConfig',function($scope,docsConfig){
         $scope.leftNavList = docsConfig.docs;
+
+        $scope.toggleLeftNav = function(){
+            $scope.toggle = !$scope.toggle;
+        };
     }]);
 })();
